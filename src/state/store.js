1e576b5c03d77861ce57508de0fd02b8cc97f4fb
tony-kornmeier <tony.kornmeier@deque.com>
2018-10-28 17:55:41 -0400

    fix: uncommented code in store.js

--- src/state/store.js
+++ src/state/store.js
@@ -87,7 +87,7 @@ store.login = action(function login(email, password) {
 });
 
 store.externalLogin = action(function externalLogin(provider) {
-  // store.startAction();
+  store.startAction();
   store.error = null;
   store.message = null;
   const url = store.invite_token
